d93150b75ede452209839d9d537dd8f926a7847d
naufal-dean <13518123@std.stei.itb.ac.id>
2022-06-29 17:23:30 +0700

    chore: add echo message

--- chatbot/fb/index.ts
+++ chatbot/fb/index.ts
@@ -1,18 +1,41 @@
-import logger from "../../utils/logger";
+import fetch from 'node-fetch';
 
-function handleMessage(senderId: string, message: object) {
+import logger from '../../utils/logger';
+
+const handleMessage = async (senderId: string, message: any) => {
     logger.info(`Handle message from ${senderId}...`);
-
+    callSendAPI(senderId, {
+        text: 'echo: ' + message.text
+    });
 }
 
-function handlePostback(senderId: string, postback: object) {
+const handlePostback = async (senderId: string, postback: any) => {
     logger.info(`Handle postback from ${senderId}...`);
 
 }
 
-function callSendAPI(senderId: string, response: object) {
-    logger.info(`Send message to ${senderId}...`);
+const callSendAPI = async (recipientId: string, message: any) => {
+    logger.info(`Send message to ${recipientId}...`);
+    
+    const payload = {
+        'recipient': {
+            'id': recipientId
+        },
+        'message': message,
+    };
+    logger.info(JSON.stringify(payload))
+    const response = await fetch('https://graph.facebook.com/v2.6/me/messages?' + new URLSearchParams({
+        access_token: process.env.PAGE_ACCESS_TOKEN || '',
+    }), {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+    });
+    const data = await response.json();
 
+    logger.info(JSON.stringify(data));
 }
 
 export { handleMessage, handlePostback, callSendAPI };
--- utils/logger.ts
+++ utils/logger.ts
@@ -3,7 +3,6 @@ import winston from 'winston';
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
     transports: [
         new winston.transports.File({ filename: '../logs/error.log', level: 'error' }),
         new winston.transports.Console({ format: winston.format.simple() }),
