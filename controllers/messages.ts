51a0e73b5d2a0de5dbb43bdd86979c073c8f63f3
naufal-dean <13518123@std.stei.itb.ac.id>
2022-06-30 06:10:05 +0700

    fix: remote __v property from messages endpoint

--- controllers/messages.ts
+++ controllers/messages.ts
@@ -4,7 +4,7 @@ import Message from '../models/message';
 const get = async (req: Request, res: Response) => {
     // Retrieve all messages
     try {
-        const messages = await Message.find();
+        const messages = await Message.find().select('-__v');
         res.status(200).json(messages);
     } catch (err) {
         res.status(500).json({
@@ -19,7 +19,7 @@ const getById = async (req: Request, res: Response) => {
 
     // Retrieve message with messageId
     try {
-        const message = await Message.findById(messageId);
+        const message = await Message.findById(messageId).select('-__v');
         res.status(200).json(message);
     } catch (err) {
         res.status(500).json({
