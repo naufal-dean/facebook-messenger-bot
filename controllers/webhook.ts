8149cf0e8b0725c07a7f437dea3d3a5464506284
naufal-dean <13518123@std.stei.itb.ac.id>
2022-06-29 12:53:15 +0700

    feat: init webhook get api

--- controllers/webhook.ts
+++ controllers/webhook.ts
@@ -1,9 +1,20 @@
 import type { Request, Response } from 'express';
 
 const get = async (req: Request, res: Response) => {
-    res.status(200).json({
-        body: 'Webhook get',
-    });
+    // Parse the query params
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    // Checks the mode and token sent is correct
+    if (mode && token && mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+    } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+    }
 }
 
 const post = async (req: Request, res: Response) => {
@@ -16,10 +27,10 @@ const post = async (req: Request, res: Response) => {
             console.log(webhook_event);
         });
         res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Not an event from page subscription, return 404
+        res.sendStatus(404);
     }
-
-    // Not an event from page subscription, return 404
-    res.sendStatus(404);
 }
 
 export default { get, post };
