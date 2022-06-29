000f93a88b5c9a760e8ac4669c1bd0da858c8243
naufal-dean <13518123@std.stei.itb.ac.id>
2022-06-29 12:44:25 +0700

    feat: init webhook post api

--- controllers/webhook.ts
+++ controllers/webhook.ts
@@ -7,9 +7,19 @@ const get = async (req: Request, res: Response) => {
 }
 
 const post = async (req: Request, res: Response) => {
-    res.status(200).json({
-        body: 'Webhook post',
-    });
+    // Checks if this is an event from a page subscription
+    if (req.body.object === 'page') {
+        // Iterates entry
+        req.body.entry.forEach(function (entry: any) {
+            // Gets the message
+            const webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    }
+
+    // Not an event from page subscription, return 404
+    res.sendStatus(404);
 }
 
 export default { get, post };
